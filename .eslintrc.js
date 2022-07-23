cee321ee78a704df99066e1a160c8ed0bfcaaa11
kevin.sjj <kevin.sjj@kakaoent.com>
2022-07-23 19:25:37 +0900

    [OS-48] INIT: project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    "prettier"
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
