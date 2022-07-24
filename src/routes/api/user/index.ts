4fc4edcc9166af92ec68189585203af0f29ea772
kevin.sjj <kevin.sjj@kakaoent.com>
2022-07-24 20:22:28 +0900

    [OS-49] 이메일, 닉네임 중복검사 API 추가

--- src/routes/api/user/index.ts
+++ src/routes/api/user/index.ts
@@ -12,9 +12,32 @@ userRouter.post("/join", async (req, res) => {
       ?.db()
       .collection(mongoController.COLLECTIONS.USER)
       .insertOne(req.body);
-    res.status(200);
-  } catch {
-    res.status(500);
+    res.status(201);
+  } catch (error) {
+    res.status(500).send(error);
+  } finally {
+    res.end();
+    mongoConnection?.close();
+  }
+});
+
+userRouter.get("/join/check-unique", async (req, res) => {
+  const { field, value } = req.query;
+  if (typeof field !== "string" || typeof value !== "string")
+    return res.status(422).end();
+  const mongoController = new MongoController();
+  let mongoConnection = null;
+  try {
+    mongoConnection = await mongoController.getConnection();
+    const hasDocument = await mongoConnection
+      ?.db()
+      .collection(mongoController.COLLECTIONS.USER)
+      .findOne({ [field]: value });
+    res.status(200).json({
+      isDup: !!hasDocument,
+    });
+  } catch (error) {
+    res.status(500).send(error);
   } finally {
     res.end();
     mongoConnection?.close();
